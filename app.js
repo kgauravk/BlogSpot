efed8962e6127ca12ce6b8be2873e5f0ba401366
kgauravk <iit2017115@iiita.ac.in>
2018-12-29 23:31:51 +0530

    setting pages to show blogs and form to add new blog

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ mongoose    = require('mongoose'),
 bodyParser  = require('body-parser');
 
 
-mongoose.connect('mongodb://localhost/blog_spot');
+mongoose.connect('mongodb://localhost/blog_spot',{useNewUrlParser:true});
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
@@ -18,13 +18,59 @@ var blogSchema = new mongoose.Schema({
 });
 var Blog = mongoose.model("Blog",blogSchema);
 
+// Blog.create({
+//     title : "The Mythbuster’s Guide to Saving Money on Energy Bills",
+//     image : "https://www.miss-thrifty.co.uk/wp-content/uploads/2018/09/38154357964_63da2f548e_z-150x150.jpg",
+//     body : "Once you get past the beginner-level energy-saving stuff, a whole new world of thrifty nerdery opens up. Here are some secrets to copping a load of money off your utilities bills",
+// },(err,blog)=>{
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log('ADDED');
+//         console.log(blog);
+//     }
+// });
+
 app.get('/',(req,res)=>{
     res.redirect('/blogs');
 });
 
 app.get('/blogs',(req,res)=>{
-    res.render('index');
-})
+
+    Blog.find({},(err,blogs)=>{
+        if(err){
+            console.log(err);
+        }else{
+            res.render('index',{blogs:blogs});
+        }
+    });
+
+});
+
+app.get('/blogs/new',(req,res)=>{
+    res.render('new');
+});
+
+app.post('/blogs',(req,res)=>{
+
+    var title = req.body.title;
+    var img = req.body.image;
+    var body = req.body.body;
+
+    Blog.create({
+        title: title,
+        image : img,
+        body :body
+    },(err,blog)=>{
+        if(err){
+            console.log(err);
+        }else{
+            console.log('ADDED');
+            console.log(blog);
+            res.redirect('/blogs');
+        }
+    });
+});
 
 app.listen(3000,()=>{
     console.log("SERVER STARTED!!!");
